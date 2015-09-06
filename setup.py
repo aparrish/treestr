db56f81edae9cadcbd8f66bcc87b0057743b42c9
hugovk <hugovk@users.noreply.github.com>
2015-09-06 20:28:11 +0300

    flake8

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # chardet's setup.py
 from distutils.core import setup
 setup(
-    name = "treestr",
-    packages = ["treestr"],
-    version = "0.0.1",
-    description = "Strings with history and tags",
-    author = "Allison Parrish",
-    author_email = "allison@decontextualize.com",
-    url = "http://github.com/aparrish/treestr",
-    download_url = "",
-    keywords = ["strings"],
-    classifiers = [
+    name="treestr",
+    packages=["treestr"],
+    version="0.0.1",
+    description="Strings with history and tags",
+    author="Allison Parrish",
+    author_email="allison@decontextualize.com",
+    url="http://github.com/aparrish/treestr",
+    download_url="",
+    keywords=["strings"],
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
@@ -20,7 +20,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Linguistic",
         ],
-    long_description = """\
+    long_description="""\
 treestr
 -------
 
